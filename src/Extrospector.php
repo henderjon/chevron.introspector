331d9cc4cf11bbe303bfaab3cc2e9cfccfed7ad9
henderjon <jhenderson@capstonedigital.com>
2014-07-16 10:38:32 -0500

    fixes misnamed method call, adds docs

--- src/Extrospector.php
+++ src/Extrospector.php
@@ -1,20 +1,38 @@
 <?php
 
 namespace Chevron\Introspector;
-
+/**
+ * adds a way of inspecting an object to which you're otherwise unable to add
+ * IntrospectorTrait.
+ *
+ * @package Chevron\Introspector
+ */
 class Extrospector {
 
 	use \Chevron\Introspector\Traits\IntrospectorTrait;
 
+	/**
+	 * the object to inspect
+	 */
 	protected $that;
 
+	/**
+	 * @param mixed $obj The object to inspect
+	 * @param bool $deep Toggle how deep to inspect
+	 */
 	function __construct($obj, $deep = false){
 		$this->that = $obj;
 		$this->setDeep($deep);
 	}
 
+	/**
+	 * override the Trait's introspect method since we're not INTROspecting
+	 */
 	public function introspect(){ /* noop */ }
 
+	/**
+	 * tirgger the inspection of the previously supplied object
+	 */
 	public function extrospect(){
 		return $this->_parseObject($this->that);
 	}
--- src/Traits/IntrospectorTrait.php
+++ src/Traits/IntrospectorTrait.php
@@ -1,19 +1,38 @@
 <?php
 
 namespace Chevron\Introspector\Traits;
-
+/**
+ * adds a way of inspecting $this. Originally meant to be used as a serializer
+ * that didn't choke on Closures. Useful for serializing complex objects for
+ * logging
+ *
+ * @package Chevron\Introspector
+ */
 trait IntrospectorTrait {
 
+	/**
+	 * whether or not to inspect sub objects
+	 */
 	protected $_deep = false;
 
+	/**
+	 * toggle a deeper dive into object properties
+	 * @param boolean $deep
+	 */
 	public function setDeep($deep){
 		$this->_deep = !!$deep;
 	}
 
+	/**
+	 * parse $this
+	 */
 	public function introspect(){
 		return $this->_parseObject($this);
 	}
 
+	/**
+	 * evaluate a property's value
+	 */
 	protected function _typeLoop($val){
 		switch(true){
 			case is_scalar($val) :
@@ -36,10 +55,18 @@ trait IntrospectorTrait {
 		}
 	}
 
+	/**
+	 * handle a scalar value
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseScalar($val){
 		return $val;
 	}
 
+	/**
+	 * handle an array value
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseArray(array $val){
 		$final = [];
 		foreach($val as $k => $v){
@@ -48,6 +75,10 @@ trait IntrospectorTrait {
 		return $final;
 	}
 
+	/**
+	 * handle a object value
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseObject($val){
 		$obj = new \ReflectionClass($val);
 		$props = $obj->getProperties();
@@ -59,21 +90,35 @@ trait IntrospectorTrait {
 		return $final ?: get_class($val);
 	}
 
+	/**
+	 * handle a resource value
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseResource($val){
 		return get_resource_type($val);
 	}
 
+	/**
+	 * handle a value not already handled
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseOther($val){
 		return gettype($val);
 	}
 
+	/**
+	 * handle an object without diving any deeper into it's object properties
+	 * @param scalar $val the value to handle
+	 */
 	protected function _parseObjectShallow($val){
 		$props = get_object_vars($val);
 		$final = [];
 		foreach($props as $k => $v){
-			$final[strtr($k, "\0", "-")] = $this->typeLoop($v);
+			$final[strtr($k, "\0", "-")] = $this->_typeLoop($v);
 		}
 		return $final ?: get_class($val);
 	}
 
 }
+
+

6648691e06bf2f30d777cbb4793e193818267a29
henderjon <jhenderson@capstonedigital.com>
2014-07-18 13:51:21 -0500

    fixes PHPUnit paths

---
+++ tests/PHPUnit/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require "vendor/autoload.php";
+
+require __DIR__ . "/stubs/IntrospectorStub.php";
+
+require __DIR__ . "/stubs/ExtrospectorStub.php";
--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "vendor/autoload.php";
-
-require "tests/stubs/IntrospectorStub.php";
-
-require "tests/stubs/ExtrospectorStub.php";
